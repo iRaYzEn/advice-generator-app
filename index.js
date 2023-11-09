465a313feddb8f9f020e24d2ff3eb0f474c6b69e
Rzan <abbassgame335463@gmail.com>
2023-11-10 00:43:58 +0300

    adding the filter varible

---
+++ index.js
@@ -0,0 +1,17 @@
+const btn = document.querySelector(".btn");
+const adviceP = document.querySelector(".advice__advice");
+const adviceNum = document.querySelector(".advice__num");
+let num = 1
+
+async function advice() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    console.log(data.slip.advice)
+    adviceP.textContent = data.slip.advice;
+}
+
+btn.addEventListener("click", () => {
+    num++
+   adviceNum.textContent = num
+   advice()
+});
